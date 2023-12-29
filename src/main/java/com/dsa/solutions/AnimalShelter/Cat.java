9c648b4c8a9fde43ed237ea1fee5419ce249235f
nurzamatnasyrkanov <nurzamat@gmail.com>
2023-12-29 16:50:08 +0600

    Tasks queue via stacks, animal shelter

---
+++ src/main/java/com/dsa/solutions/AnimalShelter/Cat.java
@@ -0,0 +1,11 @@
+package com.dsa.solutions.AnimalShelter;
+
+public class Cat extends Animal {
+  public Cat(String n) {
+    super(n);
+  }
+
+  public String name() {
+    return "Cat: " + name;
+  }
+}
---
+++ src/main/java/com/dsa/solutions/QueueviaStacks/Main.java
@@ -0,0 +1,14 @@
+package com.dsa.solutions.QueueviaStacks;
+
+class Main {
+  public static void main(String[] args) {
+    QueueViaStack newQueue = new QueueViaStack();
+    newQueue.enqueue(1);
+    newQueue.enqueue(2);
+    newQueue.enqueue(3);
+    System.out.println(newQueue.peek());
+    newQueue.enqueue(4);
+    System.out.println(newQueue.peek());
+
+  }
+}
