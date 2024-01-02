ae223c996d4843c04dc5016f40cbfedca548421b
nurzamatnasyrkanov <nurzamat@gmail.com>
2024-01-02 17:58:58 +0600

    AVL tree

---
+++ src/main/java/com/dsa/tee/avl/BinaryNode.java
@@ -0,0 +1,15 @@
+package com.dsa.tee.avl;
+
+public class BinaryNode {
+  public int value;
+  public int height;
+  public BinaryNode left;
+  public BinaryNode right;
+
+
+BinaryNode() {
+  this.height = 0;
+}
+
+
+}
