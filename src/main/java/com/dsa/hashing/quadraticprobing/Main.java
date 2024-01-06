698c57c00f6f23fd21ba9af14d82c308f1bca553
nurzamatnasyrkanov <nurzamat@gmail.com>
2024-01-06 18:20:58 +0600

    quadratic probing

---
+++ src/main/java/com/dsa/hashing/quadraticprobing/Main.java
@@ -0,0 +1,15 @@
+package com.dsa.hashing.quadraticprobing;
+
+class Main {
+  public static void main(String[] args) {
+    QuadraticProbing quadraticProbing = new QuadraticProbing(13);
+    quadraticProbing.insertKeyInHashTable("The");
+		quadraticProbing.insertKeyInHashTable("quick");
+		quadraticProbing.insertKeyInHashTable("brown");
+		quadraticProbing.insertKeyInHashTable("fox");
+		quadraticProbing.insertKeyInHashTable("over");
+		quadraticProbing.insertKeyInHashTable("lazy");
+		quadraticProbing.displayHashTable();
+
+  }
+}
