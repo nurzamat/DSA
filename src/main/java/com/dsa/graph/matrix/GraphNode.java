a255d90f97b9faf6b676e7fbfd88504c99d07c28
nurzamatnasyrkanov <nurzamat@gmail.com>
2024-01-11 19:48:25 +0600

    graph representation

---
+++ src/main/java/com/dsa/graph/matrix/GraphNode.java
@@ -0,0 +1,12 @@
+package com.dsa.graph.matrix;
+
+public class GraphNode {
+  public String name;
+  public int index;
+  public boolean isVisited = false;
+
+  public GraphNode(String name, int index) {
+    this.name = name;
+    this.index = index;
+  }
+}
