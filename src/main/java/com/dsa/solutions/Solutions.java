17304ab693134c8c3bca8ac26adfa2fcbfcaaca8
nurzamatnasyrkanov <nurzamat@gmail.com>
2024-01-29 21:02:38 +0600

    New solution for task 27. Remove Element

--- src/main/java/com/dsa/solutions/Solutions.java
+++ src/main/java/com/dsa/solutions/Solutions.java
@@ -620,6 +620,19 @@ public class Solutions {
         return k;
     }
 
+    //27. Remove Element
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+
     public static void main(String[] args){
         //1 1 1 1 1 1 2 3 4 5
         //1 1 1 1 1 1 2 2 2 2
