fc043b91d5a7f7509096d1bc0d04ccd35c057965
nurzamatnasyrkanov <nurzamat@gmail.com>
2024-03-12 23:19:03 +0600

    solution for task 35. Search Insert Position

--- src/main/java/com/dsa/solutions/Solutions.java
+++ src/main/java/com/dsa/solutions/Solutions.java
@@ -1821,6 +1821,32 @@ public class Solutions {
         return maxArea;
     }
 
+    //35. Search Insert Position
+    public int searchInsert(int[] nums, int target) {
+        if(nums == null || nums.length == 0)
+            return 0;
+
+        int left = 0;
+        int right = nums.length-1;
+        int mid = 0;
+
+        while(left <= right){
+            mid = left + (right - left)/2;
+            if(nums[mid] == target)
+                return mid;
+
+            if(nums[mid] < target){
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+
+    }
+
     class Node {
         public int val;
         public List<Node> neighbors;
