2377f8596f77b646e9242568690e4121ff62d151
nurzamatnasyrkanov <nurzamat@gmail.com>
2024-03-14 22:54:01 +0600

    draft solution for task 33. Search in Rotated Sorted Array

--- src/main/java/com/dsa/solutions/Solutions.java
+++ src/main/java/com/dsa/solutions/Solutions.java
@@ -1911,6 +1911,37 @@ public class Solutions {
         return right;
     }
 
+    //33. Search in Rotated Sorted Array
+    public int search(int[] nums, int target) {
+
+        int l=0;
+        int r=nums.length-1;
+        int mid = 0;
+
+        while(l<=r){
+            mid = l + (r-l)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[mid]<target){
+                if(nums[r]>=target)
+                    l = mid + 1;
+                else{
+                    r = mid - 1;
+                }
+            }
+            else{
+                if(nums[l]<=target)
+                    r = mid - 1;
+                else{
+                    l = mid + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
     class Node {
         public int val;
         public List<Node> neighbors;
