9de26783ba8fa0eec942852eb65a604251aa1da8
NNasyrkanov <nurzamat@gmail.com>
2024-03-27 00:23:36 +0600

    solution for task 208. Implement Trie (Prefix Tree)

--- src/main/java/com/dsa/solutions/trie/Trie.java
+++ src/main/java/com/dsa/solutions/trie/Trie.java
@@ -5,22 +5,47 @@ import java.util.Map;
 
 //208. Implement Trie (Prefix Tree)
 class Trie {
-
+    private TrieNode root;
 
     public Trie() {
-
+      this.root = new TrieNode();
     }
 
     public void insert(String word) {
-
+        TrieNode curent = root;
+        for (Character ch:word.toCharArray()) {
+            TrieNode node = curent.children.get(ch);
+            if(node == null){
+                node = new TrieNode();
+                curent.children.put(ch, node);
+            }
+            curent = node;
+        }
+        curent.endOfString = true;
     }
 
     public boolean search(String word) {
-
+        TrieNode curent = root;
+        for (Character ch:word.toCharArray()) {
+            TrieNode node = curent.children.get(ch);
+            if(node == null){
+                return false;
+            }
+            curent = node;
+        }
+        return curent.endOfString == true;
     }
 
     public boolean startsWith(String prefix) {
-
+        TrieNode curent = root;
+        for (Character ch:prefix.toCharArray()) {
+            TrieNode node = curent.children.get(ch);
+            if(node == null){
+                return false;
+            }
+            curent = node;
+        }
+        return curent.endOfString == false;
     }
 
 
