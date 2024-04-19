652b791a10411d090da35d4fa1394103572d0779
NNasyrkanov <nurzamat@gmail.com>
2024-04-19 23:25:42 +0600

    solution for task 373. Find K Pairs with Smallest Sums

--- src/main/java/com/dsa/solutions/Solutions.java
+++ src/main/java/com/dsa/solutions/Solutions.java
@@ -2479,8 +2479,8 @@ public class Solutions {
         nums[j] = temp;
     }
 
-    //373. Find K Pairs with Smallest Sums
-    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+    //373. Find K Pairs with Smallest Sums (not optimal solution)
+    public List<List<Integer>> kSmallestPairs1(int[] nums1, int[] nums2, int k) {
         int ptr1 = 0;
         int ptr2 = 0;
 
@@ -2521,7 +2521,7 @@ public class Solutions {
         return result;
     }
 
-    //373. Find K Pairs with Smallest Sums
+    //373. Find K Pairs with Smallest Sums (not optimal solution)
     public List<List<Integer>> kSmallestPairs2(int[] nums1, int[] nums2, int k) {
 
         List<List<Integer>> allPairs = new ArrayList();
@@ -2554,6 +2554,40 @@ public class Solutions {
 
     private Integer getSum(List<Integer> list){
         return list.get(0) + list.get(1);
+    }
+
+    //373. Find K Pairs with Smallest Sums
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        // Create a priority queue to hold the arrays with a comparator to prioritize by the sum of pairs.
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        // Initialize the priority queue with the first k pairs from nums1 and the first element from nums2.
+        for (int i = 0; i < Math.min(nums1.length, k); ++i) {
+            queue.offer(new int[] {nums1[i] + nums2[0], i, 0});
+        }
+
+        // Prepare a list to store the k smallest pairs.
+        List<List<Integer>> result = new ArrayList<>();
+
+        // Keep polling from the priority queue to find the next smallest pair
+        while (!queue.isEmpty() && k > 0) {
+            // Poll the smallest sum pair from the priority queue.
+            int[] currentPair = queue.poll();
+
+            // Add the new pair [nums1[index1], nums2[index2]] to the result list.
+            result.add(Arrays.asList(nums1[currentPair[1]], nums2[currentPair[2]]));
+
+            // Decrease the remaining pairs count.
+            --k;
+
+            // If there's a next element in nums2, offer the next pair from nums1 and nums2 into the priority queue.
+            if (currentPair[2] + 1 < nums2.length) {
+                queue.offer(new int[] {nums1[currentPair[1]] + nums2[currentPair[2] + 1], currentPair[1], currentPair[2] + 1});
+            }
+        }
+
+        // Return the list of k smallest pairs.
+        return result;
     }
 
     class Node {
