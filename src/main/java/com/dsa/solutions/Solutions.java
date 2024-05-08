760ad55dbbf006909c3d7b015cea36923a28f59c
NNasyrkanov <nurzamat@gmail.com>
2024-05-08 22:49:02 +0600

    solution for task 205. Isomorphic Strings

--- src/main/java/com/dsa/solutions/Solutions.java
+++ src/main/java/com/dsa/solutions/Solutions.java
@@ -2592,24 +2592,26 @@ public class Solutions {
 
     //205. Isomorphic Strings
     public boolean isIsomorphic(String s, String t) {
-        if(s == null || t == null){
-            return false;
-        }
         if(s.length() != t.length()){
             return false;
         }
-        Map<Character, Character> map = new HashMap();
+        Map<Character, Character> map1 = new HashMap<>();
+        Map<Character, Character> map2 = new HashMap<>();
 
         for(int i=0; i<s.length(); i++){
-            if(!map.containsKey(s.charAt(i))){
-                map.put(s.charAt(i), t.charAt(i));
+            if(!map1.containsKey(s.charAt(i))){
+                if(map2.containsKey(t.charAt(i)))
+                    return false;
+                map1.put(s.charAt(i), t.charAt(i));
+                map2.put(t.charAt(i), s.charAt(i));
             }
-            else if(!map.get(s.charAt(i)).equals(t.charAt(i)))
+            else if(!map1.get(s.charAt(i)).equals(t.charAt(i)))
                 return false;
         }
 
         return true;
     }
+
     class Node {
         public int val;
         public List<Node> neighbors;
