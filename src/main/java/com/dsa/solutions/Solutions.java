65307b9aafad0635a9fc8063e8d363c1fca2d1b8
NNasyrkanov <nurzamat@gmail.com>
2024-05-08 22:57:18 +0600

    solution for task 290. Word Pattern

--- src/main/java/com/dsa/solutions/Solutions.java
+++ src/main/java/com/dsa/solutions/Solutions.java
@@ -2612,6 +2612,24 @@ public class Solutions {
         return true;
     }
 
+    public boolean wordPattern(String pattern, String s) {
+
+        String[] arr = s.split(" ");
+        if(arr.length != pattern.length())
+            return false;
+        Map<Character, String> map = new HashMap<>();
+
+        for(int i=0; i<arr.length; i++){
+            if(!map.containsKey(pattern.charAt(i))){
+                map.put(pattern.charAt(i), arr[i]);
+            }
+            else if(!map.get(pattern.charAt(i)).equals(arr[i]))
+                return false;
+        }
+
+        return true;
+    }
+
     class Node {
         public int val;
         public List<Node> neighbors;
