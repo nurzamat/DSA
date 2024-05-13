84b01591b34f598e92d9eed6781d55fa8e39517f
NNasyrkanov <nurzamat@gmail.com>
2024-05-13 23:27:24 +0600

    Solution for task 219. Contains Duplicate II

--- src/main/java/com/dsa/solutions/Solutions.java
+++ src/main/java/com/dsa/solutions/Solutions.java
@@ -2689,6 +2689,30 @@ public class Solutions {
         return slowRunner == 1;
     }
 
+    //219. Contains Duplicate II
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        // Initialize a HashMap to store the value and its most recent index
+        Map<Integer, Integer> indexMap = new HashMap<>();
+
+        for (int currentIndex = 0; currentIndex < nums.length; ++currentIndex) {
+            // Use getOrDefault to find the last index of the current number.
+            // If it's not found, use a default value that is far away from any possible index.
+            int lastIndex = indexMap.getOrDefault(nums[currentIndex], -1000000);
+
+            // Check if the current index and the last index of the same value are within k steps
+            if (currentIndex - lastIndex <= k) {
+                // If so, we found a nearby duplicate and return true.
+                return true;
+            }
+
+            // Update the map with the current value and its index for the next iteration checks
+            indexMap.put(nums[currentIndex], currentIndex);
+        }
+
+        // If no nearby duplicates are found, return false.
+        return false;
+    }
+
     // Helper method to calculate the next number in the sequence.
     private int getNext(int number) {
         int sumOfSquares = 0;
