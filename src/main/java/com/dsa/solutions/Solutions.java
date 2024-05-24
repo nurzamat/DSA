c74757b809c29e6d4f38165540366a21cc2b7a09
nurzamatnasyrkanov <nurzamat@gmail.com>
2024-05-24 20:57:51 +0600

    Solution for the task 189. Rotate Array

--- src/main/java/com/dsa/solutions/Solutions.java
+++ src/main/java/com/dsa/solutions/Solutions.java
@@ -2872,6 +2872,17 @@ public class Solutions {
         return sumOfSquares;
     }
 
+    //189. Rotate Array
+    public void rotate(int[] nums, int k) {
+        for(int i=0; i<k; i++){
+            for(int j=(nums.length-k + i); j>i; j--){
+                int temp = nums[j];
+                nums[j] = nums[j-1];
+                nums[j-1] = temp;
+            }
+        }
+    }
+
     class Node {
         public int val;
         public List<Node> neighbors;
