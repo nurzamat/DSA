e053ae6b8ad83afc06d19172fe675e194c191706
nurzamatnasyrkanov <nurzamat@gmail.com>
2024-09-13 22:24:08 +0600

    139. Word Break 2nd solution

--- src/main/java/com/dsa/solutions/Solutions.java
+++ src/main/java/com/dsa/solutions/Solutions.java
@@ -3048,6 +3048,41 @@ public int findKthLargest(int[] nums, int k) {
         return false;
     }
 
+    public boolean wordBreak2(String s, List<String> wordDict) {
+        // Convert the list of words into a hash set for efficient look-up
+        Set<String> wordSet = new HashSet<>(wordDict);
+
+        // Get the length of the string 's'
+        int strLength = s.length();
+
+        // Initialize a boolean array to keep track of possible word breaks
+        // f[i] is true if first i characters of the string can be segmented into dictionary words
+        boolean[] wordBreakTracker = new boolean[strLength + 1];
+
+        // Empty string is a valid decomposition
+        wordBreakTracker[0] = true;
+
+        // Check each substring starting from length 1 to strLength
+        for (int i = 1; i <= strLength; ++i) {
+
+            // Try different break points
+            for (int j = 0; j < i; ++j) {
+
+                // If the string up to j can be broken into valid words, and the substring from j to i is in the dictionary
+                // Then, mark the position i as true in wordBreakTracker
+                if (wordBreakTracker[j] && wordSet.contains(s.substring(j, i))) {
+                    wordBreakTracker[i] = true;
+
+                    // Break out of the loop since we have found a valid break point
+                    break;
+                }
+            }
+        }
+
+        // The last entry in wordBreakTracker tells if the entire string can be segmented or not
+        return wordBreakTracker[strLength];
+    }
+
     class Node {
         public int val;
         public List<Node> neighbors;
